85c432492578680b6a93baf78aac93f0e0e93d96
Anatolii K <tol890@ukr.net>
2020-10-10 23:56:13 +0300

    added loadSong function

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Metric',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Metric-1',
+        artist: 'Jacinto Design'
+    },
+
+]
 
 // Check If Playing
 let isPlaying = false;
@@ -25,3 +53,15 @@ function pauseSong() {
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying? pauseSong() : playSong()));
 
+// Update DOM
+function loadSong(song) {
+    console.log(song.displayName);
+    title.textContent = song.displayName;
+    // console.log(song.artist);
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// On Load - Select First Song
+loadSong(songs[3]);
